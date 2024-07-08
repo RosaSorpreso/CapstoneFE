5faf67117304d4a557468cdfc8fd0af46c0f1728
RosaSorpreso <rosasorpreso@gmail.com>
2024-07-08 19:21:41 +0200

    wishlist page

--- src/app/pages/user/user-routing.module.ts
+++ src/app/pages/user/user-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
-const routes: Routes = [{ path: '', component: UserComponent }];
+const routes: Routes = [
+  { path: '', component: UserComponent },
+  { path: 'wishlist', component: WishlistComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
--- src/app/pages/user/wishlist/wishlist.component.ts
+++ src/app/pages/user/wishlist/wishlist.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { iUserComplete } from '../../../Models/i-user-complete';
+import { iUserRegistered } from '../../../Models/i-user-registered';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-wishlist',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class WishlistComponent {
 
+  user: iUserRegistered | undefined;
+  userComplete: iUserComplete | undefined;
+  userId!: number
+
+  constructor(
+    private authSvc: AuthService
+  ){}
+
+  ngOnInit(){
+    this.authSvc.user$.subscribe(user => {
+      this.user = user || undefined;
+      if(user){
+        this.userId = user.id
+        this.authSvc.getUserById(this.userId).subscribe(user => {
+          this.userComplete = user
+        })
+      }
+    })
+  }
 }
