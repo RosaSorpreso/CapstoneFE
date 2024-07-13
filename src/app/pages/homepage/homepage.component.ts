cd62b1b673e9a376e6fe2f70c932bbd6e5182d83
RosaSorpreso <rosasorpreso@gmail.com>
2024-07-13 23:18:23 +0200

    homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { iTravelComplete } from '../../Models/i-travel-complete';
 import { TravelService } from '../../services/travel.service';
+import { iCategory } from '../../Models/i-category';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-homepage',
@@ -10,21 +12,23 @@ import { TravelService } from '../../services/travel.service';
 export class HomepageComponent {
 
   travels: iTravelComplete[] = []
+  categories: iCategory[] = []
 
-  constructor(private travelSvc: TravelService){}
+  constructor(private travelSvc: TravelService, private categorySvc: CategoryService){}
 
   ngOnInit(){
     this.getAllTravels()
+
+    this.categorySvc.category$.subscribe(categories => this.categories = categories)
   }
 
   getAllTravels(){
-    this.travels = []
     this.travelSvc.travels$.subscribe(travels => {
       this.travels = this.shuffleArray(travels);
     })
   }
 
-  shuffleArray(array: any[]): any[] {
+  shuffleArray(array: iTravelComplete[]): iTravelComplete[] {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
