1870ed01cc9167572c227e234a687173a552b3b6
RosaSorpreso <rosasorpreso@gmail.com>
2024-07-13 18:10:54 +0200

    card homepage

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { iTravelComplete } from '../../Models/i-travel-complete';
+import { TravelService } from '../../services/travel.service';
 
 @Component({
   selector: 'app-homepage',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
+
+  travels: iTravelComplete[] = []
+
+  constructor(private travelSvc: TravelService){}
+
+  ngOnInit(){
+    this.getAllTravels()
+  }
+
+  getAllTravels(){
+    this.travels = []
+    this.travelSvc.travels$.subscribe(travels => {
+      this.travels = this.shuffleArray(travels);
+    })
+  }
+
+  shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
 }
